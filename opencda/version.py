d41404f319afb0d276edd3162c28b840242be7b5
DhiaNeifar <dhia.neifar@gmail.com>
2025-08-25 13:15:25 -0400

    Bump version to 0.2.0

--- opencda/version.py
+++ opencda/version.py
@@ -1,3 +1,13 @@
 """Specifies the current version number of OpenCDA."""
 
-__version__ = "0.1.3"
+"""
+    This is the old version provided by the original authors of OpenCDA.
+    __version__ = "0.1.3"
+    
+    From __version__ = "0.2.0" onwards, Dhia Neifar will implement a new version of OpenCDA.
+    First, OpenCDA works on CARLA 0.9.14. 
+    New fixes and bugs will be the focus of OpenCDA from now on. 
+    
+"""
+
+__version__ = "0.2.0"
